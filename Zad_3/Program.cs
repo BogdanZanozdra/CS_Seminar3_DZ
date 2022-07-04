67e172cb5d44dfd54479095397a12e1f3e336639
Bogdan Zanozdra <bz300788@gmail.com>
2022-07-04 21:19:18 +0300

    DZ_3

---
+++ Zad_3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+
+if (num.Length != 5)
+{
+    Console.WriteLine("Число не пятизначное!");
+}
+
+Console.WriteLine(num);
+bool IsPalindrome(string num)
+{
+    for (int i = 0; i < num.Length / 2; i++)
+    if(num[i] != num[num.Length - 1 - i]) return false;
+    return true;
+}    
+    Console.WriteLine($"Число является палиндромом {IsPalindrome(num)}");
+
